033a15e64fc3617c45fcaa7528d16c816d732431
dmitriynaumov1024 <naumov1024@gmail.com>
2022-04-15 10:29:13 +0300

    Lab02: Update regular expressions

--- Lab02/ReUtils.cs
+++ Lab02/ReUtils.cs
@@ -6,15 +6,15 @@ using System.Text.RegularExpressions;
 static class ReUtils
 {
     static string 
-        RE_EMAIL = @"(?<=(^|\s))[a-z0-9-]+@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?(?=($|\s))",
+        RE_EMAIL = @"(?<=(^|\s))[a-z0-9-]+@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?(?=($|\W))",
         RE_SURNAME_NAME = @"\b\p{Lu}\p{Ll}+\s\p{Lu}\p{Ll}+\b",
-        RE_SURNAME_INIT = @"\b\p{Lu}\p{Ll}+\s\p{Lu}\.\p{Lu}\.(?=($|\s))",
-        RE_DOMAINS_UA = @"(?<=(^|\s))(?i)([a-z]+[a-z0-9-]*[a-z0-9]\.){1,60}(com|net|org|edu|in)\.ua\b",
+        RE_SURNAME_INIT = @"\b\p{Lu}\p{Ll}+\s\p{Lu}\.\p{Lu}\.(?=($|\W))",
+        RE_DOMAINS_UA = @"(?<=(^|\s))(?i)([a-z]+[a-z0-9-]*[a-z0-9]\.){1,60}(com|net|org|edu|in)\.ua(?=($|\W))",
         RE_ENQUOT_REAL = @"(\p{Pi}|\p{Pf}|\""|\')(\+|\-)??[0-9]+\.[0-9]+(\p{Pi}|\p{Pf}|\""|\')",
         RE_ENQUOT_INT = @"(\p{Pi}|\p{Pf}|\""|\')(\+|\-)??[0-9]+(\p{Pi}|\p{Pf}|\""|\')",
         RE_ENQUOT_COMPLEX = @"(\p{Pi}|\p{Pf}|\""|\')(\s*((\+|\-)??\s*[0-9]+(\.[0-9]+)??)??\s*(\+|\-))??\s*([0-9]+(\.[0-9]+)??)??i\s*(\p{Pi}|\p{Pf}|\""|\')",
         RE_POSTAL_CODE = @"\b[0-9]{5}\b",
-        RE_POSTAL_ADDRESS = @"(?<=(^|\s))(\p{Lu}\p{Ll}+)((\s+\p{Lu}\p{Ll}+){1,2}|(\s+\p{Lu}\.\s*\p{Lu}\.))\s*\,((\w|\-|\s|\d|\.|/)+,){2,6}\s*[0-9]{5}(?=($|\s))",
+        RE_POSTAL_ADDRESS = @"(?<=(^|\s))((\p{Lu}\p{Ll}+)((\s+\p{Lu}\p{Ll}+){1,2}|(\s+\p{Lu}\.\s*\p{Lu}\.))\s*\,)??(\w|\-|\s|\d|\.){5,30},\s*((\w+\.\s??)??\d{1,4}(/\d{1,3}|\w)??){1,3},((\w|\-|\s|\d|\.){5,20},){1,3}\s*[0-9]{5}(?=($|\W))",
         RE_CSHARP_KEYWORDS = @"\b(abstract|as|base|bool|break|byte|case|catch|char|checked|class|const|continue|decimal|default|delegate|do|double|else|enum|event|explicit|extern|false|finally|fixed|float|for|foreach|goto|if|implicit|in|int|interface|internal|is|lock|long|namespace|new|null|object|operator|out|override|params|private|protected|public|readonly|ref|return|sbyte|sealed|short|sizeof|stackalloc|static|string|struct|switch|this|throw|true|try|typeof|uint|ulong|unchecked|unsafe|ushort|using|virtual|void|volatile|while)\b";
     
     public static IDictionary<string, Regex> KnownRegex = new SortedDictionary<string, Regex> {
