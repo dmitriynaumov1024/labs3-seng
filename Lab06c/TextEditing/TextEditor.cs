9ca5d92eed0d5ff1576fd51a51c906c1020d4a6f
dmitriynaumov1024 <naumov1024@gmail.com>
2022-05-04 18:09:43 +0300

    + Lab06c: Text Editor that supports rtf and html

---
+++ Lab06c/TextEditing/TextEditor.cs
@@ -0,0 +1,176 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+class TextEditor: Multiton<TextEditor>, ITextEditor
+{
+    const int HistorySize = 100;
+
+    private string fileName;
+
+    public Func<ITextEditor> CreateNewEditor { get; set; }
+    public Func<ITextEditor> CreateNewReadonly { get; set; }
+    public Action Close { get; set; }
+    public Func<OutFileProvider> SaveSelector { get; set; }
+    public Func<InFileProvider> OpenSelector { get; set; }
+    public Action<TextStats> ShowStatsPopup { get; set; }
+    public Action<string> ShowSearchPopup { get; set; }
+    public Func<string, string, bool> ShowRemovedSpacesPopup { get; set; }
+    public event Action TextChanged;
+    public event Action CaptionChanged;
+
+    private LinkedList<string> History = new LinkedList<string> { };
+    private LinkedList<string> UndoneHistory = new LinkedList<string> { };
+
+    // Must have a protected constructor!
+    protected TextEditor() {
+
+    }
+
+    public virtual string Text { 
+        get {
+            // Return the last item of the history
+            // Prevent null pointer exceptions
+            return this.History.Count > 0 ? 
+                this.History.Last.Value : 
+                String.Empty;
+        } 
+        set {
+            // To prevent recursive events & updates
+            if (value == this.Text) {
+                return;
+            }
+            // If text was set by redoing the changes, keep undo history, 
+            // otherwise discard it.
+            if (this.UndoneHistory.Count > 0 && this.UndoneHistory.Last.Value != value) {
+                this.UndoneHistory.Clear();
+            }
+            // Write to history
+            this.History.AddLast(value);
+            if (this.History.Count > HistorySize) {
+                this.History.RemoveFirst();
+            }
+            // Invoke the TextChanged event
+            this.TextChanged.Invoke();
+        } 
+    }
+
+    public string Caption {
+        get {
+            return String.Format("{0} - Bloknot", this.fileName);
+        }
+    }
+
+    public void TryUndo()
+    {
+        if (this.History.Count > 0) {
+            this.UndoneHistory.AddLast(this.History.Last.Value);
+            if (this.UndoneHistory.Count > HistorySize) {
+                this.UndoneHistory.RemoveFirst();
+            }
+            this.History.RemoveLast();
+            this.TextChanged.Invoke();
+        }
+    }
+
+    public void TryRedo()
+    {
+        if (this.UndoneHistory.Count > 0) {
+            this.Text = this.UndoneHistory.Last.Value;
+            if (this.History.Count > HistorySize) {
+                this.History.RemoveFirst();
+            }
+            this.UndoneHistory.RemoveLast();
+            this.TextChanged.Invoke();
+        }
+    }
+
+    public bool TryNew()
+    {
+        try {
+            if (this.Text.Length > 0) this.CreateNewEditor();
+            return true;
+        }
+        catch (Exception ex) {
+            return false;
+        }
+    }
+
+    public bool TryClose()
+    {
+        try {
+            Delete(this);
+            if (this.Close != null) this.Close();
+            return true;
+        }
+        catch (Exception ex) {
+            return false;
+        }
+    }
+
+    public void SetFileProvider(InFileProvider provider)
+    {
+        if (provider.Parser is PlainTextParser) {
+            this.Text = new PlainTextBuilder(provider.Parser.Chunks).ConvertToString();
+            this.fileName = provider.FileName;
+            this.CaptionChanged.Invoke();
+        }
+    }
+
+    public bool TryOpen() 
+    {
+        try {
+            InFileProvider provider = this.OpenSelector();
+            if (provider.Parser is PlainTextParser) {
+                if (this.Text.Length < 3) {
+                    this.SetFileProvider(provider);
+                    return true;
+                }
+                else {
+                    this.CreateNewEditor().SetFileProvider(provider);
+                }
+            }
+            else {
+                this.CreateNewReadonly().SetFileProvider(provider);
+            }
+            return true;
+        }
+        catch (Exception ex) {
+            return false;
+        }
+    }
+
+    public bool TrySave()
+    {
+        try {
+            OutFileProvider provider = this.SaveSelector();
+            provider.Builder.AddManyTextChunks(new PlainTextParser(this.Text).Chunks);
+            provider.Write();
+            this.fileName = provider.FileName;
+            this.CaptionChanged.Invoke();
+            return true;
+        }
+        catch (Exception ex) {
+            return false;
+        }
+    }
+
+    public void ShowStats()
+    {
+        this.ShowStatsPopup(new TextStats(this.Text, Encoding.UTF8));
+    }
+
+    public void ShowSearch()
+    {
+        this.ShowSearchPopup(this.Text);
+    }
+
+    public void ShowRemovedSpaces()
+    {
+        string newText = Utils.RemoveSpaces(this.Text);
+        if (this.ShowRemovedSpacesPopup(this.Text, newText)) {
+            this.Text = newText;
+        }
+    }
+}
---
+++ Lab06c/TextEditing/TextEditorReadonly.cs
@@ -0,0 +1,111 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+class TextEditorReadonly : Multiton<TextEditorReadonly>, ITextEditor
+{
+    private string text;
+    private string fileName;
+
+    private IEnumerable<TextChunk> chunks;
+
+    public Action Close { get; set; }
+    public Func<OutFileProvider> SaveSelector { get; set; }
+    public Action<TextStats> ShowStatsPopup { get; set; }
+    public Action<string> ShowSearchPopup { get; set; }
+
+    public event Action TextChanged;
+    public event Action CaptionChanged;
+
+    protected TextEditorReadonly ()
+    {
+
+    }
+
+    public virtual string Text { 
+        get {
+            return this.text;
+        } 
+        set {
+            this.text = value;
+            this.TextChanged.Invoke();
+        } 
+    }
+
+    public string Caption {
+        get {
+            return String.Format("{0} (Readonly) - Bloknot", this.fileName);
+        }
+    }
+
+    public void TryUndo()
+    {
+        // do nothing
+    }
+
+    public void TryRedo()
+    {
+        // do nothing
+    }
+
+    public bool TryNew()
+    {
+        // do nothing
+        return false;
+    }
+
+    public bool TryClose()
+    {
+        try {
+            Delete(this);
+            if (this.Close != null) this.Close();
+            return true;
+        }
+        catch (Exception ex) {
+            return false;
+        }
+    }
+
+    public bool TryOpen() 
+    {
+        // do nothing
+        return false;
+    }
+
+    public void SetFileProvider(InFileProvider provider)
+    {
+        this.fileName = provider.FileName;
+        this.CaptionChanged.Invoke();
+        this.chunks = provider.Parser.Chunks;
+        this.Text = new PlainTextBuilder(provider.Parser.Chunks).ConvertToString();
+    }
+
+    public bool TrySave()
+    {
+        try {
+            OutFileProvider provider = this.SaveSelector();
+            provider.Builder.AddManyTextChunks(this.chunks);
+            provider.Write();
+            return true;
+        }
+        catch (Exception ex) {
+            return false;
+        }
+    }
+
+    public void ShowStats()
+    {
+        this.ShowStatsPopup (new TextStats(this.Text, Encoding.UTF8));
+    }
+
+    public void ShowSearch()
+    {
+        this.ShowSearchPopup (this.Text);
+    }
+
+    public void ShowRemovedSpaces()
+    {
+        // do nothing
+    }
+}
